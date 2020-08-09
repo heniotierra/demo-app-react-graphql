555796d1a88926ce2f2a02b77c8ef3a57eebc533
heniotierra <heniotster@gmail.com>
2020-08-08 21:04:03 -0300

    fix CardsWrapper width for smaller devices

--- frontend/src/components/wrappers/index.js
+++ frontend/src/components/wrappers/index.js
@@ -5,6 +5,15 @@ export const CardsWrapper = styled.div`
   max-width: 936px;
   min-height: 486px;
   margin-top: 30px;
+  @media (max-width: 1169px) {
+    max-width: 703px;  
+  }
+  @media (max-width: 877px) {
+    max-width: 468px;
+  }
+  @media (max-width: 585px) {
+    max-width: 235px;
+  }
 `;
 
 export const PageWrapper = styled.div`
